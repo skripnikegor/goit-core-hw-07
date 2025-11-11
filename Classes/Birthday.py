04e54ccdfb5bc64074fde8462b38636a2ee0c501
Egor <34675756+skripnikegor@users.noreply.github.com>
2025-11-11 19:31:25 +0200

    realised function for checking birthday format in Birthday class

--- Classes/Birthday.py
+++ Classes/Birthday.py
@@ -1,10 +1,13 @@
 from .Field import Field
+from datetime import datetime
 
 class Birthday(Field):
     def __init__(self, value):
+        self.birthdate = self.__str_to_date(value)
+        
+    
+    def __str_to_date(self, value):
         try:
-            pass
-            # Додайте перевірку коректності даних
-            # та перетворіть рядок на об'єкт datetime
+            return datetime.strptime(value, "%d.%m.%Y")
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
