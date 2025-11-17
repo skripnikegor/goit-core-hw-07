7d1592fb3364ea68c977181c264a97f3eb3e958b
Egor <34675756+skripnikegor@users.noreply.github.com>
2025-11-17 09:50:49 +0200

    Fixed Birthday class
    1. Не треба було створювати в класі  Birthday   два поля  з однаковими значеннями. Там треба було створювати тільки поле value.

--- Classes/Birthday.py
+++ Classes/Birthday.py
@@ -6,10 +6,10 @@ class Birthday(Field):
     def __init__(self, value):
         pattern = r"^\d{2}\.\d{2}\.\d{4}$"
         if re.match(pattern, value):
-            self.date = value
+            super().__init__(self.value)
         else:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
-        super().__init__(self.date)
+        
 
     def __str__(self):
-        return self.date
+        return self.value
